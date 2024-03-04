1d7f8bce418d96f31b44e83d24a054ef556efc64
gitAntonio24 <rakotondrabejhonnyantonio6@gmail.com>
2024-03-04 16:32:29 +0300

    Inscription

--- GitProjectAspNet/Models/DBConnection.cs
+++ GitProjectAspNet/Models/DBConnection.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,22 @@ namespace GitProjectAspNet.Models
 {
     public class DBConnection
     {
+        public static NpgsqlConnection connectionString = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString);
+
+        public static void Inscription(Utilisateur utilisateur)
+        {
+            var req = $"INSERT INTO public.\"utilisateur\"(nomutilisateur, motdepasse, email) VALUES ('{utilisateur.NomUtilisateur}', '{utilisateur.MotdePasse}', '{utilisateur.Email}')";
+            try
+            {
+                connectionString.Open();
+                var cmd = new NpgsqlCommand(req, connectionString);
+                cmd.ExecuteNonQuery();
+                connectionString.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
