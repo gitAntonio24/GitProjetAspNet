bcc689df3efe61238b19b1ca256466600837ac52
gitAntonio24 <rakotondrabejhonnyantonio6@gmail.com>
2024-03-04 14:52:40 +0300

    Add project files.

---
+++ GitProjectAspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitProjectAspNet.MvcApplication" Language="C#" %>
---
+++ GitProjectAspNet/Models/Utilisateur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitProjectAspNet.Models
+{
+    public class Utilisateur
+    {
+        private string _nomUtilisateur;
+        private string _motDePasse;
+        private string _email;
+
+        public Utilisateur(string nomUtilisateur, string motDePasse, string email)
+        {
+            _nomUtilisateur = nomUtilisateur;
+            _motDePasse = motDePasse;
+            _email = email;
+        }
+
+        public Utilisateur() { }
+     
+        public string  Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string MotDePasse
+        {
+            get { return _motDePasse; }
+            set { _motDePasse = value; }
+        }
+
+        public string NomUtilisateur
+        {
+            get { return _nomUtilisateur; }
+            set { _nomUtilisateur = value; }
+        }
+
+    }
+}
