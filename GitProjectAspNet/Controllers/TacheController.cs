8cfbbf24948a540e492546842fbd644088df0f56
Harys-Randria <harysrandrianomenjanahary@gmail.com>
2024-03-05 14:44:15 +0300

    grandModif

---
+++ GitProjectAspNet/Controllers/TacheController.cs
@@ -0,0 +1,19 @@
+﻿using GitProjectAspNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitProjectAspNet.Controllers
+{
+    public class TacheController : Controller
+    {
+        // GET: Tache
+        public ActionResult Tache()
+        {
+            List<Tache> _tache = DBconnectionTache.AfficherListeTache();
+            return View(_tache);
+        }
+    }
+}
---
+++ GitProjectAspNet/Models/DBconnectionTache.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Windows.Documents;
+
+namespace GitProjectAspNet.Models
+{
+    public class DBconnectionTache
+    {
+        public static NpgsqlConnection connectionString = new NpgsqlConnection(
+           ConfigurationManager.ConnectionStrings["dataBaseConnection"].ConnectionString);
+
+        public static void InsererTache() { }
+
+        public static List<Tache> AfficherListeTache() { 
+            
+            List<Tache> liste = new List<Tache>();
+
+            return liste;
+        }
+    }
+}
